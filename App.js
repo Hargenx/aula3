c4e60d4447c335ea596b6c4edddfe125b92e7053
Raphael Mauricio Sanches de Jesus <55360548+Hargenx@users.noreply.github.com>
2023-08-29 15:12:58 -0300

    👍

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: Math.random().toString(), name: newTask }]);
+      setNewTask('');
+    }
+  };
+
+  const completeTask = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: true } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Digite uma nova tarefa"
+        value={newTask}
+        onChangeText={setNewTask}
+        style={styles.input}
+      />
+      <Button title="Adicionar" onPress={addTask} />
+      <FlatList
+        data={tasks}
+        keyExtractor={(task) => task.id}
+        renderItem={({ item }) => (
+          <View style={styles.task}>
+            <Text style={item.completed ? styles.completedTask : null}>
+              {item.name}
+            </Text>
+            {!item.completed && (
+              <>
+                <Button title="Concluído" onPress={() => completeTask(item.id)} />
+                <Button title="Excluir" onPress={() => deleteTask(item.id)} />
+              </>
+            )}
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 10,
+  },
+  task: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 5,
+  },
+  completedTask: {
+    textDecorationLine: 'line-through',
   },
 });
